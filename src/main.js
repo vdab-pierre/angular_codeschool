c0ecf8aede80eb19fc5904e3768839f332ea0d97
Pierre Colin <pierre.colin@ad.core.vdabantwerpen.intra>
2017-02-17 11:22:20 +0100

    firstcomponent

--- src/main.js
+++ src/main.js
@@ -1,5 +1,45 @@
 "use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_module_1 = require('./app/app.module');
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core'); //angular library,
+//component is a function that enables us to create components, basic building blocks of an angular2 app
+//a component controls a portion of our screen
+var platform_browser_1 = require('@angular/platform-browser'); //needed for running Angular websites
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic'); //Angular library that will render the website, launch the app
+//a component consists of decorator code and a class
+var AppComponent = (function () {
+    function AppComponent() {
+    }
+    AppComponent = __decorate([
+        //Angular library that will render the website, launch the app
+        core_1.Component({
+            selector: 'my-app',
+            template: '<h1>Ultra Racing</h1>'
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppComponent);
+    return AppComponent;
+}());
+//main module that loads our components
+var AppModule = (function () {
+    function AppModule() {
+    }
+    AppModule = __decorate([
+        core_1.NgModule({
+            declarations: [AppComponent],
+            imports: [platform_browser_1.BrowserModule],
+            bootstrap: [AppComponent] //the component that gets loaded first
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppModule);
+    return AppModule;
+}());
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(AppModule); //load our AppModule
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,23 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {NgModule,Component} from '@angular/core'; //angular library,
+//component is a function that enables us to create components, basic building blocks of an angular2 app
+//a component controls a portion of our screen
+import {BrowserModule} from '@angular/platform-browser'; //needed for running Angular websites
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';//Angular library that will render the website, launch the app
 
-import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+//a component consists of decorator code and a class
+@Component({
+    selector:'my-app', //the css selector for the html el we want the component to load
+    template:'<h1>Ultra Racing</h1>'
+}) //use to apply our component to our class:typescript feature
+class AppComponent{}
+
+//main module that loads our components
+@NgModule({
+    declarations:[AppComponent],
+    imports:[BrowserModule],//loads the required dependencies
+    bootstrap:[AppComponent]//the component that gets loaded first
+})
+class AppModule{}
+
+platformBrowserDynamic().bootstrapModule(AppModule);//load our AppModule
